7ec931d1a229f8ff32b398d685c731c32bd7ecf3
Dayo Abdul <73638760+Dayo1900@users.noreply.github.com>
2022-05-16 09:30:35 +0100

    Create homepage.php

---
+++ v39-bears-team-11FE-b/public/web-pages/homepage.php
@@ -0,0 +1,54 @@
+<?php session_start();
+session_unset();
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head style="background-color: white;">
+<title>Talker</title>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="'Talker', 'chat', 'social media', 'friends', 'messaging'">
+
+<link rel="stylesheet" type="text/css" href="../src/css/talker.css">
+<link rel="stylesheet" type="text/css" media="(max-width:1280px)" href="../src/css/talker1280px.css">
+<link rel="stylesheet" type="text/css" media="(max-width:1100px)" href="../src/css/talker1100px.css">
+<link rel="stylesheet" type="text/css" media="(max-width:900px)" href="../src/css/talker900px.css">
+<link rel="stylesheet" type="text/css" media="(max-width:699px)" href="../src/css/talker699px.css">
+<link rel="stylesheet" type="text/css" media="(max-width:639px)" href="../src/css/talker639px.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+</head>
+<body class="homepage">
+    
+
+<div class="pictureframe">
+<div class="container">
+<img class="resize" src="../src/images/logo.jpg" alt="Talker" title="Talker logo">
+</div>
+</div>
+
+<div class= "line"> </div>
+
+<div class="pictureframe2">
+<img class="resize" src=" ../src/images/lady.jpg" alt="Lady chatting on Talker" title="Lady chatting on Talker">
+</div>
+
+<div class="space"> </div>
+
+<div class="description">
+<p class="description"> 
+Keep in touch with friends and family from anywhere in the world. 
+
+
+<br><br>
+<a class="linking" href="signup-login.php"> Let's talk</a>
+</p>
+</div>
+
+</body>
+</html>
+
+
+
